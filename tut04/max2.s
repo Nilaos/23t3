8c6d33af864e7563f4529c285f40acfc618c58cc
Nicholas Sims <n.sims@unsw.edu.au>
2023-10-05 09:06:37 +1100

    wk4solns

---
+++ tut04/max2.s
@@ -0,0 +1,98 @@
+# Recursive maximum of array function
+
+# Register usage:
+#   - `a' is in a0, address of start of array
+#   - `length' is in a1, elements left in array
+#   - `first_element' is in $s0
+#   - `max_so_far' is in $t1
+
+
+# if you use an $s you need to save and load it to preserve any value in it. 
+# after a function call, assume that all $a, $t registers have no meaningful values
+
+# int max(int a[], int length) {
+max:
+	# prologue
+	push	$ra
+	push	$s0
+	push	$s1
+
+	# int first_element = a[0];
+	lw	$s0, 0($a0)		# 
+
+	bne	$a1, 1, max__recursive	# if $a1 != 1 then recurse
+max__length_1:
+	# BASE CASE
+	# if (length == 1) {
+	# return first_element;
+	move 	$v0, $s0		# $v0 = first_element from $s0
+	b	max__return
+
+max__recursive:
+	# // find max value in rest of array
+	# int max_so_far = max(&a[1], length - 1);
+	# a0 and a1 need to updated for the function call
+	
+	# &a[1] = &a[0] + sizeof(int)
+	add	$a0, $a0, 4	# $a0 = $a0 + 4
+
+	# length - 1
+	add	$a1, $a1, -1	# $a1 = $a1 + -1
+
+	# Before this line: $ra is at line 72
+	jal	max
+	# After that line: $ra is 41
+	# Assume now that all $t reg are 
+	
+	move	$t1, $v0	# max_so_far = function call result
+
+	# if (first_element > max_so_far) {
+	bgt	$s0, $t1, max__swap_max_so_far	# if $s0 > $t1 then target
+
+	# else
+	move	$v0, $t1
+	b	max__return
+	
+max__swap_max_so_far:
+	# max_so_far = first_element;
+	move	$v0, $s0
+
+max__return:
+	# epilogue - MUST MATCH PROLOGUE
+	# clean up stack frame
+
+	pop	$s1 # Reverse order!
+	pop 	$s0
+	pop	$ra
+	jr	$ra
+
+
+
+# some test code which calls max
+main:
+	addi	$sp, $sp, -4	# create stack frame
+	sw	$ra, 0($sp)	# save return address
+
+	la	$a0, array
+	li	$a1, 10
+	jal	max#
+
+	move	$a0, $v0 # printf ("%d")
+	li	$v0, 1
+	syscall
+
+	li	$a0, '\n' # printf("%c", '\n');
+	li	$v0, 11
+	syscall
+
+		# clean up stack frame
+	lw	$ra, 0($sp)	# restore $ra
+	addi	$sp, $sp, 4	# restore sp
+
+	li	$v0, 0 # return 0
+	jr	$ra
+
+
+.data
+array:
+	.word 1, 2, 3, 4, 5, 6, 4, 3, 2, 1
---
+++ tut04/print_flag.s
@@ -0,0 +1,62 @@
+
+FLAG_COLS = 12
+FLAG_ROWS = 6
+
+.data
+# char flag[6][12] = {
+flag: 
+	.byte '#', '#', '#', '#', '#', '.', '.', '#', '#', '#', '#', '#'
+	.byte '#', '#', '#', '#', '#', '.', '.', '#', '#', '#', '#', '#'
+	.byte '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'
+	.byte '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'
+	.byte '#', '#', '#', '#', '#', '.', '.', '#', '#', '#', '#', '#'
+	.byte '#', '#', '#', '#', '#', '.', '.', '#', '#', '#', '#', '#'
+# };
+
+.text
+# int main(void) {
+main:
+	#     row is $t0
+	li	$t0, 0			# $t0 = 0
+	li	$t6, FLAG_ROWS		# $t6 = 6 maximum rows
+	
+main__row_loop_cond:
+						# while(row < 6) {
+	bge	$t0, $t6, main__row_loop_end	# if $t0 > $t6 then main__row_loop_end
+
+	li	$t1, 0			# $t1 = 0
+
+main__col_loop_cond:
+	bge	$t1, FLAG_COLS, main__col_loop_end	# if $t1 > FLAG_COLS goto target
+
+	# offset calculation in $t3 - TODO:
+	# 1) Row offset
+	add	$t3, $0, $t0	# Get the row number
+	mul	$t3, $t3, FLAG_COLS # Multiply row * N_COLS to get the size of a row
+	add	$t3, $t3, $t1	# 2) Add Column Offset * sizeof(char)
+				# if this wasn't chars, we'd also multiply by 4
+
+	lb	$t4, flag($t3)		# load word from 0($s1) into $t1
+
+	move	$a0, $t4		# $a0 = $41
+	li	$v0, 11			# $t1 = 1
+	syscall
+
+	addi	$t1,	$t1,	1
+
+	b	main__col_loop_cond	# branch to main__col_loop_cond
+
+main__col_loop_end:
+
+	# printf ("\n");
+	# print a character - syscall 11
+	li	$v0, 11		# $v0 =11 
+	li	$a0, '\n'	# $a0 ='\n' 
+	syscall
+
+	# row++;
+	addi	$t0, $t0, 1		# $t0 = $t0+ 1 
+	b	main__row_loop_cond	# branch to target
+
+main__row_loop_end:
+	jr		$ra		# jump to $ra
