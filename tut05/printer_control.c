f0c9bd78f0d0e27e68bd2c14e531119a5de814f5
Nicholas Sims <n.sims@unsw.edu.au>
2023-10-12 10:27:10 +1100

    tut05 solns

--- tut05/printer_control.c
+++ tut05/printer_control.c
@@ -12,27 +12,28 @@
 
 // Printer flags as #defines
 // Whether the printer is out of ink
-#define NO_INK (0x1) // 0b 0000 0001
+#define NO_INK (0x1)  // 0b 0000 0001
 // Whether to print/scan in colour
-#define COLOUR (0x2) // 0b 0000 0010
+#define COLOUR (0x2)  // 0b 0000 0010
 // Select print mode
-#define SELECT_PRINT (0x4) // 0b 0000 0100
+#define SELECT_PRINT (0x4)  // 0b 0000 0100
 // Select scan mode
-#define SELECT_SCAN (0x8) // 0b 0000 1000
+#define SELECT_SCAN (0x8)  // 0b 0000 1000
 // Start print/scan
-#define START (0x10) // 0b 0001 0000
+#define START (0x10)  // 0b 0001 0000
 
 // The printer
-uint8_t printerControl = 0; // 0b 0000 0000
+uint8_t printerControl = 0;  // 0b 0000 0000
 
-void checkInk(void);               // Question A
-void replaceInk(void);             // Question B
-void useColourAndSelectScan(void); // Question C
-void toggleMode(void);             // Question D
-void start(void);                  // Question E
+void checkInk(void);                // Question A
+void replaceInk(void);              // Question B
+void useColourAndSelectScan(void);  // Question C
+void toggleMode(void);              // Question D
+void start(void);                   // Question E
 
 // Main function for testing
-int main(void) {
+int main(void)
+{
     printf("printerControl = 0x%02x\n", printerControl);
     checkInk();
     replaceInk();
@@ -43,30 +44,45 @@ int main(void) {
 }
 
 // Prints whether the printer has ink
-void checkInk(void) {
-    // TODO
+void checkInk(void)
+{
+    // Check the NO_INK bit
+    if (NO_INK & printerControl) {
+        // printer is out of ink
+    }
 }
 
 // Assume the user has replaced the ink
 // So, we tell the printer there's ink again
-void replaceInk(void) {
-    // TODO
+void replaceInk(void)
+{
+    // Make NO_INK zero, keep all others
+    // or operator
+    // printerControl = printerControl ^ NO_INK;
+    printerControl = printerControl & (~NO_INK)
 }
 
 // Select colour and scan mode
 // Assume no mode has been selected yet
-void useColourAndSelectScan(void) {
-    // TODO
+void useColourAndSelectScan(void)
+{
+    //
+    uint8_t mask = COLOUR | SELECT_SCAN;
+    printerControl |= mask;
 }
 
 // Toggle between print and scan mode
 // Assume a mode has already been selected
-void toggleMode(void) {
+void toggleMode(void)
+{
     // TODO
+    uint8_t mask = SELECT_PRINT | SELECT_SCAN;
+    printerControl = printerControl ^ mask;
 }
 
 // Start printing/scanning and print relevant messages
-void start(void) {
+void start(void)
+{
     // Check that only 1 mode is selected
     // Double bang to boolean-ise our bitwise results
     // Has stdbool.h been introduced??
