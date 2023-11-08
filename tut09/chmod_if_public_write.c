425fa0e7032a2d622b5c6c6e51b798702ee68518
Nicholas Sims <nicholas.d.sims@gmail.com>
2023-11-09 00:40:50 +1100

    wk9 starter code

---
+++ tut09/chmod_if_public_write.c
@@ -0,0 +1,17 @@
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 1; i < argc; i++)
+    {
+        char *curr_file = argv[i];
+
+        
+    }
+    return 0;
+}
