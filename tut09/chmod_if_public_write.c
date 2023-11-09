f75f9802783e45ef6f11f308e7e76fcc679c335f
Nicholas Sims <n.sims@unsw.edu.au>
2023-11-09 15:15:40 +1100

    tut09 soln

--- tut09/chmod_if_public_write.c
+++ tut09/chmod_if_public_write.c
@@ -7,11 +7,33 @@
 
 int main(int argc, char const *argv[])
 {
-    for (int i = 1; i < argc; i++)
-    {
+    for (int i = 1; i < argc; i++) {
         char *curr_file = argv[i];
 
-        
+        // todo: get permissions
+        struct stat statbuf;
+        int err = lstat(curr_file, &statbuf);
+        if (err) {
+            perror(curr_file);
+            exit(1);
+        }
+        // todo: some error checking
+        mode_t oldmode = statbuf.st_mode;
+
+        if (oldmode & S_IWOTH) {
+            // it is pub writable - change that
+            mode_t newmode = oldmode & (~S_IWOTH);
+            err = chmod(curr_file, newmode);
+            if (err) {
+                perror(curr_file);
+                exit(1);
+            }
+
+            fprintf(stdout, "Changed mode of %s to 0%o", curr_file, newmode);
+        } else {
+            // not pub writable
+            fprintf(stdout, "%s is not publically writable.", curr_file);
+        }
     }
     return 0;
 }
